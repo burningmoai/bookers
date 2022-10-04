422b8f63f5ea70c24a7395a8d883a3e0de2a2c33
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-10-04 05:51:27 +0000

    [Add]各アクションへフラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,18 +19,20 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.save
     flash[:notice]="Book was successfully created."
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice]="Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice]="Book was successfully destroyed."
     redirect_to '/books'
   end
 
